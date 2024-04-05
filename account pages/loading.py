07d4f526367ca8d985c863a776d807acc7a833e2
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-04-05 07:32:51 +0500

    loading page

--- account pages/loading.py
+++ account pages/loading.py
@@ -6,12 +6,15 @@ from tkinter import ttk
 
 # root window
 root = Tk()
+image=PhotoImage(file="account pages\wel.jpg")
+image=Label(root,image=image)
+image.place(x=0,y=0)
 
 hight = 430
 width = 530
-x = (root.winfo_screenwidth() // 2) - (width // 2)  # center the window
-y = (root.winfo_screenheight() // 2) - (hight // 2)  # center the window
-root.geometry("{}x{}+{}+{}".format(width, hight, x, y))  # set the window size and position
+x = (root.winfo_screenwidth() // 2) - (width // 2)  
+y = (root.winfo_screenheight() // 2) - (hight // 2) 
+root.geometry("{}x{}+{}+{}".format(width, hight, x, y))
 root.overrideredirect(1)  # remove the title bar
 root.configure(bg="#2f6c60")
 
@@ -20,8 +23,7 @@ exit_button = Button(root, text="X", bg="#2f6c60", fg="white", command=lambda: E
 exit_button.place(x=0, y=0)
 
 # each label
-welcome_label = Label(root, text="Welcome to My app \n Accord-\"Ask about Islam\" ", bg="#2f6c60", fg="white",
-                      font=("Arial", 20))
+welcome_label = Label(root, text="Welcome to My app \n Accord-\"Ask about Islam\" ", bg="#2f6c60", fg="white",font=("Arial", 20))
 welcome_label.place(x=100, y=70)
 
 progressLabel = Label(root, text="Loading...", bg="#2f6c60", fg="white", font=("Arial", 10))
@@ -36,7 +38,7 @@ def Exit_window():
 
 def top():
     root.withdraw()
-    os.system('python loadingPage.py')
+    os.system('python loadingpage.py')
     root.destroy()
 
 i=0
@@ -51,6 +53,6 @@ def load():
     else:
         top()
 
-load()  # Call the load function to start the progress bar
+load()
 
 root.mainloop()
---
+++ account pages/loadingpage.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window=Tk()
+window.title("home Page")
+
+welcome_label = Label(window, text="Welcome to My app \n Accord-\"Ask about Islam\" ", bg="#2f6c60", fg="white")
+welcome_label.pack()
+
+window.mainloop()
