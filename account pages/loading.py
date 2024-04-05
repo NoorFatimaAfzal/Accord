492a97b6a8ec3380e644e0066fad61776de904ec
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-04-05 09:42:43 +0500

    started working on sign up and login page

--- account pages/loading.py
+++ account pages/loading.py
@@ -4,54 +4,91 @@ import sys
 import os
 from tkinter import ttk
 
-# root window
+# Root window
 root = Tk()
-image=PhotoImage(file="account pages\wel.jpg")
-image=Label(root,image=image)
-image.place(x=0,y=0)
+current_dir = os.path.dirname(os.path.realpath(__file__))
+image_path = os.path.join(current_dir, "vector.png")
+image = PhotoImage(file=image_path)
 
-hight = 430
 width = 530
-x = (root.winfo_screenwidth() // 2) - (width // 2)  
-y = (root.winfo_screenheight() // 2) - (hight // 2) 
-root.geometry("{}x{}+{}+{}".format(width, hight, x, y))
-root.overrideredirect(1)  # remove the title bar
+height = 430
+root.wm_attributes('-fullscreen', True)
 root.configure(bg="#2f6c60")
 
-# each button
-exit_button = Button(root, text="X", bg="#2f6c60", fg="white", command=lambda: Exit_window(), bd=0)
-exit_button.place(x=0, y=0)
+# Exit button
+top_frame = Frame(root, bg="#2f6c60")
+top_frame.pack(fill=X)
+exit_button = Button(
+    top_frame,
+    text="X",
+    bg="#2f6c60",
+    fg="white",
+    command=lambda: Exit_window(),
+    bd=0,
+)
+exit_button.pack(side=RIGHT, padx=10, pady=10)
 
-# each label
-welcome_label = Label(root, text="Welcome to My app \n Accord-\"Ask about Islam\" ", bg="#2f6c60", fg="white",font=("Arial", 20))
-welcome_label.place(x=100, y=70)
+# Welcome label
+welcome_label = Label(
+    root,
+    text="Welcome to My App\n Accord - \"Ask about Islam\"",
+    bg="#2f6c60",
+    fg="white",
+    font=("Arial", 20)
+)
+welcome_label.pack(fill=X, pady=20)
 
-progressLabel = Label(root, text="Loading...", bg="#2f6c60", fg="white", font=("Arial", 10))
-progressLabel.place(x=200, y=200)
 
-progress = ttk.Progressbar(root, orient=HORIZONTAL, length=300, mode="determinate")
-progress.place(x=100, y=250)
+image_label = Label(root, image=image)
+image_label.pack(pady=20)
 
-# each process (I mean functions)
+# progress bar
+bottom_frame = Frame(root, bg="#2f6c60")
+bottom_frame.pack(fill=X, expand=True)
+progressLabel = Label(
+    bottom_frame,
+    text="Loading...",
+    bg="#2f6c60",
+    fg="white",
+    font=("Arial", 10)
+)
+progressLabel.pack(side=BOTTOM, pady=10)
+
+progress = ttk.Progressbar(
+    bottom_frame,
+    orient=HORIZONTAL,
+    length=300,
+    mode="determinate"
+)
+progress.pack(side=BOTTOM, pady=10)
+
+# Functions
 def Exit_window():
     sys.exit(root.destroy())
 
+
 def top():
     root.withdraw()
-    os.system('python loadingpage.py')
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    loadingpage_path = os.path.join(current_dir, "signup page.py")
+    os.system(f'python "{loadingpage_path}"')
     root.destroy()
 
-i=0
+
+i = 0
+
+
 def load():
     global i
-    if i <10:
-        txt="Loading..."+str(10*i)+"%"
+    if i < 10:
+        txt = "Loading..." + str(10 * i) + "%"
         progressLabel.config(text=txt)
-        progressLabel.after(600,load)
-        progress['value']=10*i
-        i+=1
+        progressLabel.after(600, load)
+        progress['value'] = 10 * i
+        i += 1
     else:
         top()
+
 
 load()
 
