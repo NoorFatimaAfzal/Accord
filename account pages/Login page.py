b3136cc19bc7c8d59e7492aea304302ca8ae7ab8
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-04-06 12:30:06 +0500

    starting to create login page

---
+++ account pages/Login page.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.ttk import Progressbar
+from PIL import Image, ImageTk
+from tkinter import ttk
+import os
+from tkinter import messagebox
+
+Login_window=Tk()
+
+
+Login_window.mainloop()
--- account pages/signup page.py
+++ account pages/signup page.py
@@ -5,35 +5,37 @@ from tkinter import ttk
 import os
 from tkinter import messagebox
 
-login_window=Tk()
-login_window.geometry("990x660+50+50")
-login_window.configure(bg="white")
-login_window.resizable(False, False)   
+SignUp_window=Tk()
+SignUp_window.geometry("990x660+50+50")
+SignUp_window.configure(bg="white")
+SignUp_window.resizable(False, False)   
 
 #functions
 def user_enter(event):
     if usernameEntry.get() == "Username":
         usernameEntry.delete(0, END)
-def passward_enter(event):
-    if passwordEntry.get() == "Password":
-        passwordEntry.delete(0, END)
-def Confirm_passward_enter(event):
-    if passwordEntry.get() == "Confirm Password":
-        passwordEntry.delete(0, END)
+
+def login():
+    SignUp_window.withdraw()
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    loginPage_path = os.path.join(current_dir, "Login page.py")
+    os.system(f'python "{loginPage_path}"')
+    SignUp_window.destroy()
+    
 
 # image
 current_dir = os.path.dirname(os.path.realpath(__file__))
 image_path = os.path.join(current_dir, "vector.png")
 bgImage=ImageTk.PhotoImage(file=image_path)
-bgLabel=Label(login_window,image=bgImage,background="white")
+bgLabel=Label(SignUp_window,image=bgImage,background="white")
 bgLabel.place(x=0, y=0)
 
 # title
-title=Label(login_window,text="Sign Up", font=("Arial", 20, "bold"), bg="sky blue", fg="black")
+title=Label(SignUp_window,text="Sign Up", font=("Arial", 20, "bold"), bg="sky blue", fg="black")
 title.place(x=650, y=120, anchor="center")
 
 # email
-emailEntry=Entry(login_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
+emailEntry=Entry(SignUp_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
 emailEntry.place(x=557, y=170, anchor="center") 
 emailEntry.insert(0, "Email")
 
@@ -48,19 +50,19 @@ def email_leave(event):
 
 emailEntry.bind("<FocusIn>",email_enter)
 emailEntry.bind("<FocusOut>",email_leave)
-frameEmail=Frame(login_window, width=260, height=2,background="black") 
+frameEmail=Frame(SignUp_window, width=260, height=2,background="black") 
 frameEmail.place(x=660, y=195, anchor="center")
 
 # username
-usernameEntry=Entry(login_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
+usernameEntry=Entry(SignUp_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
 usernameEntry.place(x=580, y=220, anchor="center")
 usernameEntry.insert(0, "Username")
 usernameEntry.bind("<FocusIn>",user_enter)
-frame1=Frame(login_window, width=260, height=2,background="black")
+frame1=Frame(SignUp_window, width=260, height=2,background="black")
 frame1.place(x=660, y=245, anchor="center")
 
 # password 
-passwordEntry=Entry(login_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
+passwordEntry=Entry(SignUp_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
 passwordEntry.place(x=580, y=270, anchor="center")
 passwordEntry.insert(0, "Password")
 
@@ -70,11 +72,11 @@ def passward_enter(event):
         passwordEntry.config(show="*")
 
 passwordEntry.bind("<FocusIn>",passward_enter)
-frame2=Frame(login_window, width=260, height=2,background="black")
+frame2=Frame(SignUp_window, width=260, height=2,background="black")
 frame2.place(x=660, y=295, anchor="center") 
 
 # confirm password 
-confirmPasswordEntry=Entry(login_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
+confirmPasswordEntry=Entry(SignUp_window,width=25, font=("Arial", 15),bd=0, bg="white", fg="black", relief=FLAT, justify=CENTER)
 confirmPasswordEntry.place(x=615, y=330, anchor="center")
 confirmPasswordEntry.insert(0, "Confirm Password")
 
@@ -84,12 +86,12 @@ def confirm_passward_enter(event):
         confirmPasswordEntry.config(show="*")
 
 confirmPasswordEntry.bind("<FocusIn>",confirm_passward_enter)
-frame3=Frame(login_window, width=260, height=2,background="black")  
+frame3=Frame(SignUp_window, width=260, height=2,background="black")  
 frame3.place(x=660, y=345, anchor="center") 
 
 # Privacy Policy Checkbutton
 privacyPolicyVar = StringVar()
-privacyPolicyCheck = Checkbutton(login_window, text="I agree to the Privacy Policy", variable=privacyPolicyVar, onvalue="Yes", offvalue="No", bg="white", fg="black", font=("Arial", 12))  
+privacyPolicyCheck = Checkbutton(SignUp_window, text="I agree to the Privacy Policy", variable=privacyPolicyVar, onvalue="Yes", offvalue="No", bg="white", fg="black", font=("Arial", 12))  
 privacyPolicyCheck.place(x=670, y=380, anchor="center")  
 
 # Sign Up Button
@@ -99,21 +101,20 @@ def signup_click():
     else:
         pass
 
-signupButton = Button(login_window, text="Sign Up", font=("Arial", 15, "bold"), bg="sky blue", fg="black", command=signup_click)
+signupButton = Button(SignUp_window, text="Sign Up", font=("Arial", 15, "bold"), bg="sky blue", fg="black", command=signup_click)
 signupButton.place(x=650, y=429, anchor="center")
 
 # Already a member? Login
 def open_login(event):
-    login_window.destroy()
-    pass
+    login()
 
-loginLink = Label(login_window, text="Already a member? Login", bg="white", fg="black", cursor="hand2", font=("Arial", 15)) 
+loginLink = Label(SignUp_window, text="Already a member? Login", bg="white", fg="black", cursor="hand2", font=("Arial", 15)) 
 loginLink.place(x=650, y=470, anchor="center")
 loginLink.bind("<Button-1>", open_login)
 
-# loginButton = Button(login_window, text="Already a member? Login", bg="white", fg="black", font=("Arial", 15), command=open_login)
+# loginButton = Button(SignUp_window, text="Already a member? Login", bg="white", fg="black", font=("Arial", 15), command=open_login)
 # loginButton.place(x=650, y=470, anchor="center")
 
 
 
-login_window.mainloop()
+SignUp_window.mainloop()
