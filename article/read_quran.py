aa926f904a49e8815b75a03aeeb0dde455793418
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-04-28 23:10:20 +0500

    testing phase

--- article/read_quran.py
+++ article/read_quran.py
@@ -2,70 +2,40 @@ import tkinter as tk
 import requests
 import pygame.mixer
 import os
+import tempfile
+import shutil
 
-# Initialize the mixer module
+
 pygame.mixer.init()
 
 root = tk.Tk()
 root.title("Quran Surahs")
 
-# Function to fetch Surah data from API
 def fetch_data():
     response = requests.get('http://api.alquran.cloud/v1/surah')
     data = response.json()
     return data['data']
 
-# Fetch Surah data from API
 quran_data = fetch_data()
 
-
-
-# Function to handle Surah selection
 def surah_selected(event):
-    # Enable the Text widget
     ayah_text.config(state=tk.NORMAL)
     selected_surah = surah_list.curselection()[0]
     surah_id = quran_data[selected_surah]['number']
     response = requests.get(f'http://api.alquran.cloud/v1/surah/{surah_id}/en.asad')
     surah_data = response.json()['data']
-    # Clear the Text widget
     ayah_text.delete('1.0', tk.END)
-    # Insert "Bismillah" before each Surah except Surah At-Tawbah
     if surah_id != 9:
         ayah_text.insert(tk.END, "بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ\n\n", 'center')
-    # Insert each Ayah into the Text widget
     for ayah in surah_data['ayahs']:
         ayah_text.insert(tk.END, f"Ayah {ayah['numberInSurah']}:", 'bold')
         ayah_text.insert(tk.END, f" {ayah['text']}\n\n")
-    # Disable the Text widget
     ayah_text.config(state=tk.DISABLED)
     global audio_urls
     audio_urls = [ayah['audio'] for ayah in audio_data['surahs'][selected_surah]['ayahs']]
 
-
-# Frame for Ayah labels and Scrollbars
-ayah_frame = tk.Frame(root)
-ayah_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-
-
-import tempfile
-import shutil
-
-# Initialize the mixer module
-pygame.mixer.init()
-
-# Fetch the audio data from the API
-audio_response = requests.get('https://api.alquran.cloud/v1/quran/ar.alafasy')
-audio_data = audio_response.json()['data']
-
-# Define a custom event type for when a music stream ends
-SONG_END = pygame.USEREVENT + 1
-pygame.mixer.music.set_endevent(SONG_END)
-
-# Function to play audio
 def play_audio():
     if audio_urls:
-        # Download the first audio file to a temporary file
         audio_url = audio_urls.pop(0)
         audio_response = requests.get(audio_url, stream=True)
         if audio_response.status_code == 200:
@@ -76,7 +46,32 @@ def play_audio():
             pygame.mixer.music.load(audio_path)
             pygame.mixer.music.play()
 
-# Vertical Scrollbar
+def go_back():
+    with open('previous_page.txt', 'r') as f:
+        previous_page = f.read().strip()
+    root.withdraw()
+    os.system(f'python "{previous_page}"')
+    root.destroy()
+
+def open_help(page):
+    with open('previous_page.txt', 'w') as f:
+        f.write(page)
+    root.withdraw()
+    os.system('python "C:\\Users\\InfoBay\\OneDrive\\Desktop\\Accord\\help\\help.py"')
+    root.destroy()
+
+
+ayah_frame = tk.Frame(root)
+ayah_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+pygame.mixer.init()
+
+audio_response = requests.get('https://api.alquran.cloud/v1/quran/ar.alafasy')
+audio_data = audio_response.json()['data']
+
+SONG_END = pygame.USEREVENT + 1
+pygame.mixer.music.set_endevent(SONG_END)
+
 vscrollbar = tk.Scrollbar(ayah_frame)
 vscrollbar.grid(row=1, column=3, sticky='ns')
 
@@ -104,26 +99,6 @@ surah_list.bind("<<ListboxSelect>>", surah_selected)
 
 # Initialize the Pygame video system
 pygame.display.init()
-
-# Function to go back to the previous page
-def go_back():
-    # Read the previous page from the file
-    with open('previous_page.txt', 'r') as f:
-        previous_page = f.read().strip()
-    # Withdraw the current window
-    root.withdraw()
-    # Open the previous page
-    os.system(f'python "{previous_page}"')
-    # Destroy the current window
-    root.destroy()
-
-def open_help(page):
-    with open('previous_page.txt', 'w') as f:
-        f.write(page)
-    root.withdraw()
-    os.system('python "C:\\Users\\InfoBay\\OneDrive\\Desktop\\Accord\\help\\help.py"')
-    root.destroy()
-
 
 # Play Audio Button
 play_button = tk.Button(ayah_frame, text="Play Audio", font=("Arial", 15), bg="sky blue", fg="black",  command=play_audio)
